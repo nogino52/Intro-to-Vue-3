795f313636068e2315d00b18806af3a0b6fe2b18
nogino <nogino52@outlook.jp>
2021-11-10 20:18:46 +0900

    complete L8

--- main.js
+++ main.js
@@ -4,21 +4,33 @@ const app = Vue.createApp({
             cart:0,
             product: 'Socks',
             brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            selectedVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 0 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 50 },
+            ],
+            onSale: true
         }
     },
     methods: {
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index;
+        }
+    },
+    computed: {
+        title() 
+        {
+            return this.brand + " " + this.product + (this.onSale ? " is on sale" : "");
+        },
+        image() {
+            return this.variants[this.selectedVariant].image;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity > 0;
         }
     }
 })
